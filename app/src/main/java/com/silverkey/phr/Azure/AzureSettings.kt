fcbbc49414cfe8dc0d13b046de7c5ac901d9b9df
Omar Mahmmoud Salem <omarsalem977@gmail.com>
2018-06-21 14:47:55 +0200

    Update AzureSettings.kt

--- app/src/main/java/com/silverkey/phr/Azure/AzureSettings.kt
+++ app/src/main/java/com/silverkey/phr/Azure/AzureSettings.kt
@@ -1,13 +1,6 @@
 package com.silverkey.phr.Azure
 
-/**
- * Created by Mohamed Taher on 2/5/2018.
- */
+
 class AzureSettings {
-    var storageAccountName: String? = "stratusphr"//"devstoreaccount1"
-    var accessKey: String? = "OWbe5RLb9jxxE+UFOZt/8yiv+S/e9gpxfa3xb8kLVGRXuYRKx8MU3LbrIQURciqkXfZHVpOK1KFif+s/FE4Ifg=="//"Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
-    var blobUri: String? = "https://stratusphr.blob.core.windows.net/"
-    var queueUri: String? = "https://stratusphr.blob.core.windows.net/"
-    var tableUri: String? = "https://stratusphr.blob.core.windows.net/"
-    var fileUri: String? = "https://stratusphr.blob.core.windows.net/"
+   // This class contains all access tool in Azure Cloud like access key etc .
 }
