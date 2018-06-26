94730c261546ed7d72a8999ea9a39c1d55105e0f
Omar Mahmmoud Salem <omarsalem977@gmail.com>
2018-06-26 15:43:31 +0200

    Update Config.kt

--- app/src/main/java/com/silverkey/phr/API/Config.kt
+++ app/src/main/java/com/silverkey/phr/API/Config.kt
@@ -2,12 +2,13 @@ package com.silverkey.scops.API
 
 /**
  * Created by Mohamed Taher on 12/12/2017.
+ * Updated by Omar Salem on  5/12/2018
  */
 object Config {
 
-    const val BASE_API = "https://stratushealth.azurewebsites.net/"
-    const val REGISTERATION = "Api/User"
-    const val LOGIN = "Api/User/Login"
-    const val GET_SHAREABLE_LINK = "Api/File/GetShareableLink"
+    const val BASE_API = "***********************"
+    const val REGISTERATION = "*************"
+    const val LOGIN = "***********"
+    const val GET_SHAREABLE_LINK = "**************"
 
 }
